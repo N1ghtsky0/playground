b40b010fbc55a3d75b300991ad4d58d14c46736d
N1ghtsky0 <20korea01@gmail.com>
2023-11-30 15:04:33 +0900

    feat: init SecurityConfig

---
+++ src/main/java/jiwook/kim/playground/base/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package jiwook.kim.playground.base.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    private final String[] ALLOWED_URL_PERMIT_ALL = {"/", "/error"};
+    private final String[] ALLOWED_URL_CSRF = {"/api/**"};
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrfConfigurer -> csrfConfigurer.ignoringRequestMatchers(ALLOWED_URL_CSRF))
+                .authorizeHttpRequests(requests ->
+                        requests.requestMatchers(ALLOWED_URL_PERMIT_ALL).permitAll()
+                )
+                .build();
+    }
+}
