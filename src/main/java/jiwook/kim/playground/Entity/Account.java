4dee1c5690d8bf882e7e12df1f8596c3bf546735
N1ghtsky0 <20korea01@gmail.com>
2023-11-30 16:41:24 +0900

    feat: Account 생성 및 BaseTimeEntity 적용

---
+++ src/main/java/jiwook/kim/playground/Entity/Account.java
@@ -0,0 +1,68 @@
+package jiwook.kim.playground.Entity;
+
+import jakarta.persistence.*;
+import jiwook.kim.playground.base.common.AccountType;
+import jiwook.kim.playground.base.common.BaseTimeEntity;
+import jiwook.kim.playground.dto.request.RequestSignUp;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Account extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long seq;
+
+    @Column(nullable = false, length = 20)
+    private String loginId;
+
+    @Column(nullable = false)
+    private String loginPwd;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+    @Column(length = 20)
+    private String nickName;
+
+    @Column(length = 10)
+    private String birthDay;
+
+    @Enumerated(EnumType.STRING)
+    private AccountType type;
+
+    @Column(length = 36)
+    private String uuid;
+
+    public static Account createUser(RequestSignUp requestSignUp) {
+        return Account.builder()
+                .loginId(requestSignUp.getLoginId())
+                .loginPwd(requestSignUp.getLoginPwd())
+                .name(requestSignUp.getName())
+                .nickName(requestSignUp.getNickName())
+                .birthDay(requestSignUp.getBirthDay())
+                .type(AccountType.USER)
+                .uuid(UUID.randomUUID().toString())
+                .build();
+    }
+
+    public static Account createAdmin(RequestSignUp requestSignUp) {
+        return Account.builder()
+                .loginId(requestSignUp.getLoginId())
+                .loginPwd(requestSignUp.getLoginPwd())
+                .name(requestSignUp.getName())
+                .nickName(requestSignUp.getNickName())
+                .birthDay(requestSignUp.getBirthDay())
+                .type(AccountType.ADMIN)
+                .build();
+    }
+
+}
