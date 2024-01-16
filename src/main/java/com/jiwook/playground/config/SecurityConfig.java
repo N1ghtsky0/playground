04d67d5f486655bfb15ddf4bf60587137ec964d6
N1ghtsky0 <20korea01@gmail.com>
2024-01-16 21:51:28 +0900

    feat: SecurityConfig

---
+++ src/main/java/com/jiwook/playground/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.jiwook.playground.config;
+
+import com.jiwook.playground.model.UserRole;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    private final String[] ALL_ALLOWED_URLS = {"/", "/index", "/login"};
+    private final String[] USER_ALLOWED_URLS = {"/info"};
+    private final String[] ADMIN_ALLOWED_URLS = {"/users"};
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request.requestMatchers(ALL_ALLOWED_URLS).permitAll()
+                        .requestMatchers(USER_ALLOWED_URLS).hasAnyAuthority(UserRole.USER.name(), UserRole.ADMIN.name())
+                        .requestMatchers(ADMIN_ALLOWED_URLS).hasAuthority(UserRole.ADMIN.name()));
+        return http.build();
+    }
+}
